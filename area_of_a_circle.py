b2f37fa0040f99dbe98b7f365ddde42c2035e52b
test <test@gmail.com>
2025-04-09 10:22:28 -0500

    This is an update for the in class lab

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -10,3 +10,5 @@ print(f"The area of circle with radius {radius1} is: {area1}")
 radius2 = 40
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
+
+#This is a test comment to add for the GitHub in class lab that I am doing
